003d90b57350eb0a3683b9b3b0e97a4562be3d09
Najiba I <78580746+najibaI@users.noreply.github.com>
2025-12-14 00:43:23 -0500

    Update DAMF_applied_MRI.m

--- src/DAMF_applied_MRI.m
+++ src/DAMF_applied_MRI.m
@@ -2,25 +2,25 @@
 % Original Image
 I4 = imread('mri_image.jpg');
 
-% Convert to grayscale
+% Converting to grayscale
 if ndims(I4) == 3
     I4 = rgb2gray(I4);
 end
 
-% Convert to uint8
+% Converting to uint8
 I4 = uint8(I4);
 
-% Add salt and pepper noise
+% Adding salt and pepper noise
 noise_density = 0.1;  % 10% density noise level
 % tested out 60% density noise level using the line below in replacement of
 % 10% density level
 % noise_density = 0.6;  % 60% density noise level
 noisy_image = imnoise(I4, 'salt & pepper', noise_density);
 
-% Apply DAMF filter
+% Applying DAMF filter
 filtered_image = DAMF(noisy_image);
 
-% Display results
+% Displaying results
 figure(1);
 imshow(I4);
 title('I4: Original MRI Image', "FontSize", 15, "Color", "Black");
@@ -36,4 +36,5 @@ title('I6: DAMF Filtered Image', "FontSize", 15, "Color", "Black");
 % Saving image outputs
 imwrite(noisy_image, 'mri_image_noisy.png');
 imwrite(filtered_image, 'mri_image_DAMF_filtered.png');
+
 
