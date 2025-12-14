fe3dac8a629fc6ad487621eee8d4b0a69edc5d47
Janice <janiceyezhu@gmail.com>
2025-12-13 19:16:20 -0500

    add gaussian noise

--- src/noise_modeling.m
+++ src/noise_modeling.m
@@ -1,15 +1,15 @@
-%% Salt and Pepper Noise
+%% MRI Image
 I0 = imread('brain_mri.jpg');
-noise_density=0.1;
-I1 = imnoise(I0,"salt & pepper",noise_density);
+I0_gray = im2gray(I0);
 figure('color','white');
-subplot(2,1,1);
-imshow(I0);
-title('I0: MRI Image');
+imshow(I0_gray);
+%% Salt and Pepper Noise
+noise_density=0.1;
+I1_0 = imnoise(I0_gray,"salt & pepper",noise_density);
+figure('color','white');
+imshow(I1_0);
 
-subplot(2,1,2);
-imshow(I1);
-title({'I1: Salt and Pepper Noise','in MRI Image (d=0.1)'});
-
-%% Thermal Noise
 %% Gaussian Noise
+sigma = 0.01;
+I1_2 = imnoise(I0_gray, 'gaussian', 0, sigma);
+imshow(I1_2);
